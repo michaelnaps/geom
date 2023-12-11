00ae8602dfd1ac9ddcfc39115c7c654521327a6c
Michael Napoli <mnapoli@irobot.com>
2023-12-11 09:56:37 -0500

    lower dens.

--- levelset.py
+++ levelset.py
@@ -5,7 +5,7 @@ from GEOM.LevelSet import *
 if __name__ == '__main__':
     bound = 10
     F = lambda x: (x.T@x)[0][0]
-    lset = LevelSet( F, 5, 0.05, [-bound,bound], [-bound,bound], tol=1e-22 ).draw()
+    lset = LevelSet( F, 5, 0.1, [-bound,bound], [-bound,bound], tol=1e-22 ).draw()
     lset.axs.axis( 'equal' )
     lset.axs.axis( [-5, 5, -5, 5] )
     plt.show( block=0 )
